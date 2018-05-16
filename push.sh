d66f9a7d587fc398f6707c053567f872edb247a7
vishwaroopa rangamgari <yqa_cal05@yahoo.com>
2018-05-16 16:56:03 -0700

    test

--- push.sh
+++ push.sh
@@ -1,2 +1,4 @@
-rm -rf sdk_build_examples; cp -R /Users/charyr/installs/source/sdk_build_examples .
+#rm -rf sdk_build_examples; cp -R /Users/charyr/installs/source/sdk_build_examples .
+#cp /Users/charyr/installs/source/sherpa/server/test/serverTest .
+cp /Users/charyr/installs/source/sherpa/sherpa .
 git add *; git commit -m"test"; git push
